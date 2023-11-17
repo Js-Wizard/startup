5a78f23e0789ad9abc33c8362c5a44285a139264
Brent Thomson <brent.alex.thomson@gmail.com>
2023-11-17 09:26:19 -0700

    Set up auth packages

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
