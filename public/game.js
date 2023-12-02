0d9650fd452dc139572a4e3e409870303cdd48b8
Brent Thomson <brent.alex.thomson@gmail.com>
2023-12-02 16:38:10 -0700

    Adapt ws code to this game

--- public/game.js
+++ public/game.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// Event messages
+const WinEvent = 'win';
+const LossEvent = 'loss';
+
 class Game
 {
     constructor()
@@ -174,26 +178,21 @@ class Game
     configureWebSocket() {
         const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
         this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
-        this.socket.onopen = (event) => {
-          this.displayMsg('system', 'game', 'connected');
-        };
-        this.socket.onclose = (event) => {
-          this.displayMsg('system', 'game', 'disconnected');
-        };
+
         this.socket.onmessage = async (event) => {
           const msg = JSON.parse(await event.data.text());
-          if (msg.type === GameEndEvent) {
-            this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
-          } else if (msg.type === GameStartEvent) {
-            this.displayMsg('player', msg.from, `started a new game`);
+          if (msg.type === WinEvent) {
+            this.displayMsg(msg.from, `won in ${msg.value} turns!`);
+          } else if (msg.type === LossEvent) {
+            this.displayMsg(msg.from, `lost...`);
           }
         };
       }
     
-      displayMsg(cls, from, msg) {
-        const chatText = document.querySelector('#player-messages');
-        chatText.innerHTML =
-          `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
+      displayMsg(from, msg) {
+        const logText = document.querySelector('.log');
+        logText.innerHTML =
+        `<li><span class="player-name">${from}</span> ${msg}</li>`; + chatText.innerHTML;
       }
     
       broadcastEvent(from, type, value) {
