f31d8a2e17c08ee44e5f9a307b8b736189cc411c
Brent Thomson <brent.alex.thomson@gmail.com>
2023-12-02 16:25:06 -0700

    Insert simon client ws

--- public/game.js
+++ public/game.js
@@ -23,6 +23,8 @@ class Game
         this.guestName = localStorage.getItem("2p-name");
         if (!this.guestName) { this.guestName = "Player 2"; }
         this.reset();
+
+        this.configureWebSocket();
     }
 
     reset()
@@ -166,6 +168,42 @@ class Game
 
         this.options.style = "display: block";
     }
+
+
+
+    configureWebSocket() {
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+        this.socket.onopen = (event) => {
+          this.displayMsg('system', 'game', 'connected');
+        };
+        this.socket.onclose = (event) => {
+          this.displayMsg('system', 'game', 'disconnected');
+        };
+        this.socket.onmessage = async (event) => {
+          const msg = JSON.parse(await event.data.text());
+          if (msg.type === GameEndEvent) {
+            this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
+          } else if (msg.type === GameStartEvent) {
+            this.displayMsg('player', msg.from, `started a new game`);
+          }
+        };
+      }
+    
+      displayMsg(cls, from, msg) {
+        const chatText = document.querySelector('#player-messages');
+        chatText.innerHTML =
+          `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
+      }
+    
+      broadcastEvent(from, type, value) {
+        const event = {
+          from: from,
+          type: type,
+          value: value,
+        };
+        this.socket.send(JSON.stringify(event));
+      }
 }
 
 const game = new Game();
