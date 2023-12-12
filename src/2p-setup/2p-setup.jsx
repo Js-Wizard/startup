4040fc4428d33d405fce6d62e96bd188101eebfc
Brent Thomson <brent.alex.thomson@gmail.com>
2023-12-11 17:36:45 -0700

    Create view components

---
+++ src/2p-setup/2p-setup.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function TwoPSetup() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>2p-setup displayed here</div>
+    </main>
+  );
+}
---
+++ src/mode-select/mode-select.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function ModeSelect() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>mode-select displayed here</div>
+    </main>
+  );
+}
