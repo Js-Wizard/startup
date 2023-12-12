dab68d4ca8a8d73f77a285deff1ece00908d73c3
Brent Thomson <brent.alex.thomson@gmail.com>
2023-12-11 17:31:32 -0700

    Put starting html in app comp

--- src/app.jsx
+++ src/app.jsx
@@ -4,5 +4,20 @@ import './app.css';
 
 export default function App()
 {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return <div className='body bg-dark text-light'>
+    <header>
+        <a href="home.html">
+            <div className="logo"><img alt="fim logo" src="logo.png" /></div>
+        </a>
+    </header>
+
+    <main>App components go here</main>
+
+    <footer>
+        <span className="author">
+            &copy; Brent Thomson 2023 | 
+            <a href="https://github.com/Js-Wizard/startup">Source</a>
+        </span>
+    </footer>
+  </div>;
 }
