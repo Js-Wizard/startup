434c51abf2136b198ac0d836e723bb61416e6809
Brent Thomson <brent.alex.thomson@gmail.com>
2023-12-12 21:30:09 -0700

    Port mode select to React

--- src/mode-select/mode-select.jsx
+++ src/mode-select/mode-select.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
+import {useNavigate} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
 export function ModeSelect() {
-  return (
-    <main>
-      <div>mode-select displayed here</div>
-    </main>
-  );
+    const navigate = useNavigate();
+
+    function play1p()
+    {
+        localStorage.setItem("player-count", 1);
+        navigate('/play');
+    }
+
+    return (
+        <main>
+            <section>
+                <h1>Choose game mode</h1>
+                <menu>
+                    <li><Button variant="success" onClick={play1p}>Play against CPU</Button></li>
+                    <li><Link to="/2p-setup"><Button variant="primary">Local 2-player</Button></Link></li>
+                    <li><Link to="/home"><Button variant="outline-light">Back</Button></Link></li>
+                </menu>
+            </section>
+        </main>
+    );
 }
